1b3cc4bf3308f8b29fe0bdebb9ac28a519990fc0
StanlyHung825 <stanly940825@gmail.com>
2026-05-27 20:58:01 +0800

    Add pilz motion planner to enable planning move through sequence of waypoints to increase the rate

--- ws/src/myplan/launch/demo.launch.py
+++ ws/src/myplan/launch/demo.launch.py
@@ -3,5 +3,12 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("robotarm", package_name="myplan").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("robotarm", package_name="myplan")
+        .planning_pipelines(
+            default_planning_pipeline="pilz_industrial_motion_planner",
+            pipelines=["pilz_industrial_motion_planner", "ompl"],
+        )
+        .to_moveit_configs()
+    )
     return generate_demo_launch(moveit_config)
